e4a4ddab10c4693029c4ef796bc5aa23479c50a7
Steve Adrian Santana <StevenAdrianSantana@hotmail.com>
2025-04-29 19:52:27 -0400

    Visible

--- SIGDEN/Form1.cs
+++ SIGDEN/Form1.cs
@@ -16,5 +16,10 @@ namespace SIGDEN
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
     }
 }
