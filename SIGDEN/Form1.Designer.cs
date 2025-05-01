94eb42ccd24f7b888ecc03b54db1e88e03645210
Diicarlo <phoebynunez26@gmail.com>
2025-05-01 17:56:39 -0400

    Cambio Eri

--- SIGDEN/Form1.Designer.cs
+++ SIGDEN/Form1.Designer.cs
@@ -36,19 +36,21 @@ namespace SIGDEN
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(302, 101);
+            this.label1.Location = new System.Drawing.Point(226, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 55);
+            this.label1.Size = new System.Drawing.Size(102, 44);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Puta";
+            this.label1.Text = "Hola";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
