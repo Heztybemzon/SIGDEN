7c70433ed6f20a5a3433301f0c03b7ff56040f46
Diicarlo <phoebynunez26@gmail.com>
2025-05-26 06:19:37 -0400

    Actualización final con cambios aceptados del compañero

--- SIGDEN/FrmHome.Designer.cs
+++ SIGDEN/FrmHome.Designer.cs
@@ -254,7 +254,7 @@ namespace SIGDEN
             // 
             this.guna2PictureBox1.Image = global::SIGDEN.Properties.Resources.Logoo;
             this.guna2PictureBox1.ImageRotate = 0F;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(525, -11);
+            this.guna2PictureBox1.Location = new System.Drawing.Point(527, -11);
             this.guna2PictureBox1.Name = "guna2PictureBox1";
             this.guna2PictureBox1.Size = new System.Drawing.Size(489, 155);
             this.guna2PictureBox1.TabIndex = 0;
