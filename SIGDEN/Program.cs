6a768840c744c3f136b02c4e82dc0706ca78b57e
Steve Adrian Santana <StevenAdrianSantana@hotmail.com>
2025-05-26 11:46:38 -0400

    Cambios S

--- SIGDEN/Program.cs
+++ SIGDEN/Program.cs
@@ -16,7 +16,7 @@ namespace SIGDEN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Gastro_Protectores());
+            Application.Run(new Tabla_Pacientes());
         }
     }
 }
